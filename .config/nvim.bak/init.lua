7097898ca2df9d70ebb8bde44d390e448bf52e7d
peter bull <petercbull@gmail.com>
2024-10-04 09:08:21 -0400

    add lazyvim config

---
+++ .config/nvim.bak/init.lua
@@ -0,0 +1,2 @@
+vim.opt.clipboard = "unnamedplus"
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {noremap=true, silent=true})
---
+++ .config/nvim/lua/plugins/undotree.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "mbbill/undotree",
+    keys = {
+      { "<leader>u", vim.cmd.UndotreeToggle, desc = "Toggle Undotree" },
+    },
+    cmd = "UndotreeToggle",
+  },
+}
