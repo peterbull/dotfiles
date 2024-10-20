1c85a02cf2049c45ab4006e2e4aa4c258d48d7e1
peter bull <petercbull@gmail.com>
2024-10-20 10:28:50 -0400

    nvim and tmux updates

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      style = "cool",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
--- .config/nvim/lua/plugins/lspsaga.lua
+++ .config/nvim/lua/plugins/lspsaga.lua
@@ -2,6 +2,8 @@ return {
   "nvimdev/lspsaga.nvim",
   event = "LspAttach",
   config = function()
+    -- Sets bulbs to always visible to prevent visual stutter
+    vim.opt.signcolumn = "yes:2"
     require("lspsaga").setup({})
     -- Custom keymaps
     vim.keymap.set("n", "gt", "<cmd>Lspsaga peek_type_definition<CR>", { desc = "Peek type definition" })
