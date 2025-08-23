987459005cfe180361ce8f0616b05ab0abb0eb92
peter bull <17122363+peterbull@users.noreply.github.com>
2025-08-23 09:07:03 -0400

    working debug setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -166,6 +166,13 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+vim.g.python_recommended_style = 0
+vim.g.rust_recommended_style = 0
+vim.opt.tabstop = 2
+vim.opt.smartindent = true
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
@@ -234,6 +241,24 @@ end
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
+---@param config {type?:string, args?:string[]|fun():string[]?}
+local function get_args(config)
+  local args = type(config.args) == 'function' and (config.args() or {}) or config.args or {} --[[@as string[] | string ]]
+  local args_str = type(args) == 'table' and table.concat(args, ' ') or args --[[@as string]]
+
+  config = vim.deepcopy(config)
+  ---@cast args string[]
+  config.args = function()
+    local new_args = vim.fn.expand(vim.fn.input('Run with args: ', args_str)) --[[@as string]]
+    if config.type and config.type == 'java' then
+      ---@diagnostic disable-next-line: return-type-mismatch
+      return new_args
+    end
+    return require('dap.utils').splitstr(new_args)
+  end
+  return config
+end
+--
 -- [[ Configure and install plugins ]]
 --
 --  To check the current status of your plugins, run
@@ -967,6 +992,419 @@ require('lazy').setup({
     --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  },
+
+  -- Debug setup
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      -- Creates a beautiful debugger UI
+      'rcarriga/nvim-dap-ui',
+      -- virtual text for the debugger
+      { 'theHamsta/nvim-dap-virtual-text', opts = {} },
+      -- Mason integration for DAP
+      {
+        'jay-babu/mason-nvim-dap.nvim',
+        dependencies = 'mason.nvim',
+        cmd = { 'DapInstall', 'DapUninstall' },
+        opts = {
+          automatic_installation = true,
+          handlers = {},
+          ensure_installed = {
+            'js-debug-adapter',
+          },
+        },
+      },
+      -- Ensure js-debug-adapter is installed via Mason
+      {
+        'williamboman/mason.nvim',
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          table.insert(opts.ensure_installed, 'js-debug-adapter')
+        end,
+      },
+    },
+    keys = {
+      {
+        '<F5>',
+        function()
+          require('dap').continue()
+        end,
+        desc = 'Debug: Start/Continue',
+      },
+      {
+        '<F1>',
+        function()
+          require('dap').step_into()
+        end,
+        desc = 'Debug: Step Into',
+      },
+      {
+        '<F2>',
+        function()
+          require('dap').step_over()
+        end,
+        desc = 'Debug: Step Over',
+      },
+      {
+        '<F3>',
+        function()
+          require('dap').step_out()
+        end,
+        desc = 'Debug: Step Out',
+      },
+      {
+        '<F7>',
+        function()
+          require('dapui').toggle()
+        end,
+        desc = 'Debug: Toggle UI',
+      },
+      {
+        '<leader>dB',
+        function()
+          require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+        end,
+        desc = 'Breakpoint Condition',
+      },
+      {
+        '<leader>db',
+        function()
+          require('dap').toggle_breakpoint()
+        end,
+        desc = 'Toggle Breakpoint',
+      },
+      {
+        '<leader>dc',
+        function()
+          require('dap').continue()
+        end,
+        desc = 'Run/Continue',
+      },
+      {
+        '<leader>da',
+        function()
+          require('dap').continue { before = get_args }
+        end,
+        desc = 'Run with Args',
+      },
+      {
+        '<leader>dC',
+        function()
+          require('dap').run_to_cursor()
+        end,
+        desc = 'Run to Cursor',
+      },
+      {
+        '<leader>dg',
+        function()
+          require('dap').goto_()
+        end,
+        desc = 'Go to Line (No Execute)',
+      },
+      {
+        '<leader>di',
+        function()
+          require('dap').step_into()
+        end,
+        desc = 'Step Into',
+      },
+      {
+        '<leader>dj',
+        function()
+          require('dap').down()
+        end,
+        desc = 'Down',
+      },
+      {
+        '<leader>dk',
+        function()
+          require('dap').up()
+        end,
+        desc = 'Up',
+      },
+      {
+        '<leader>dl',
+        function()
+          require('dap').run_last()
+        end,
+        desc = 'Run Last',
+      },
+      {
+        '<leader>do',
+        function()
+          require('dap').step_out()
+        end,
+        desc = 'Step Out',
+      },
+      {
+        '<leader>dO',
+        function()
+          require('dap').step_over()
+        end,
+        desc = 'Step Over',
+      },
+      {
+        '<leader>dP',
+        function()
+          require('dap').pause()
+        end,
+        desc = 'Pause',
+      },
+      {
+        '<leader>dr',
+        function()
+          require('dap').repl.toggle()
+        end,
+        desc = 'Toggle REPL',
+      },
+      {
+        '<leader>ds',
+        function()
+          require('dap').session()
+        end,
+        desc = 'Session',
+      },
+      {
+        '<leader>dt',
+        function()
+          require('dap').terminate()
+        end,
+        desc = 'Terminate',
+      },
+      {
+        '<leader>dw',
+        function()
+          require('dap.ui.widgets').hover()
+        end,
+        desc = 'Widgets',
+      },
+      {
+        '<leader>dL',
+        function()
+          require('dap.ext.vscode').load_launchjs()
+        end,
+        desc = 'Load launch.json',
+      },
+    },
+    config = function()
+      local dap = require 'dap'
+
+      -- Enable DAP logging for debugging issues
+      dap.set_log_level 'INFO'
+
+      -- Setup pwa-node adapter (the modern way)
+      dap.adapters['pwa-node'] = {
+        type = 'server',
+        host = 'localhost',
+        port = '${port}',
+        executable = {
+          command = 'node',
+          args = {
+            vim.fn.stdpath 'data' .. '/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js',
+            '${port}',
+          },
+        },
+      }
+
+      -- Setup node adapter (compatibility layer)
+      dap.adapters['node'] = function(cb, config)
+        if config.type == 'node' then
+          config.type = 'pwa-node'
+        end
+        local nativeAdapter = dap.adapters['pwa-node']
+        if type(nativeAdapter) == 'function' then
+          nativeAdapter(cb, config)
+        else
+          cb(nativeAdapter)
+        end
+      end
+
+      -- Setup Chrome adapter
+      dap.adapters['pwa-chrome'] = {
+        type = 'server',
+        host = 'localhost',
+        port = '${port}',
+        executable = {
+          command = 'node',
+          args = {
+            vim.fn.stdpath 'data' .. '/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js',
+            '${port}',
+          },
+        },
+      }
+
+      local js_filetypes = { 'typescript', 'javascript', 'typescriptreact', 'javascriptreact' }
+
+      -- Setup vscode compatibility
+      local vscode = require 'dap.ext.vscode'
+      vscode.type_to_filetypes['node'] = js_filetypes
+      vscode.type_to_filetypes['pwa-node'] = js_filetypes
+      vscode.type_to_filetypes['pwa-chrome'] = js_filetypes
+
+      -- Setup configurations for each filetype
+      for _, language in ipairs(js_filetypes) do
+        dap.configurations[language] = {
+          {
+            type = 'pwa-node',
+            request = 'launch',
+            name = 'Launch file',
+            program = '${file}',
+            cwd = '${workspaceFolder}',
+          },
+          {
+            type = 'pwa-node',
+            request = 'attach',
+            name = 'Attach',
+            processId = require('dap.utils').pick_process,
+            cwd = '${workspaceFolder}',
+          },
+        }
+      end
+
+      -- Auto-load launch.json when entering a directory
+      vim.api.nvim_create_autocmd('DirChanged', {
+        callback = function()
+          local launch_json = vim.fn.getcwd() .. '/.vscode/launch.json'
+          if vim.fn.filereadable(launch_json) == 1 then
+            require('dap.ext.vscode').load_launchjs(launch_json, {
+              ['pwa-node'] = js_filetypes,
+              ['pwa-chrome'] = js_filetypes,
+              ['node'] = js_filetypes,
+              ['chrome'] = js_filetypes,
+            })
+            print('Auto-loaded: ' .. launch_json)
+          end
+        end,
+      })
+
+      -- Also load launch.json on startup if it exists
+      local startup_launch_json = vim.fn.getcwd() .. '/.vscode/launch.json'
+      if vim.fn.filereadable(startup_launch_json) == 1 then
+        require('dap.ext.vscode').load_launchjs(startup_launch_json, {
+          ['pwa-node'] = js_filetypes,
+          ['pwa-chrome'] = js_filetypes,
+          ['node'] = js_filetypes,
+          ['chrome'] = js_filetypes,
+        })
+      end
+
+      -- Set up highlights
+      vim.api.nvim_set_hl(0, 'DapStoppedLine', { default = true, link = 'Visual' })
+
+      -- Set up DAP signs
+      local dap_icons = {
+        Stopped = { '󰁕 ', 'DiagnosticWarn', 'DapStoppedLine' },
+        Breakpoint = ' ',
+        BreakpointCondition = ' ',
+        BreakpointRejected = { ' ', 'DiagnosticError' },
+        LogPoint = '.>',
+      }
+      for name, sign in pairs(dap_icons) do
+        vim.fn.sign_define('Dap' .. name, {
+          text = sign[1],
+          texthl = sign[2] or 'DiagnosticInfo',
+          linehl = sign[3],
+          numhl = sign[3],
+        })
+      end
+
+      -- Debug function to check adapter status
+      vim.api.nvim_create_user_command('DapStatus', function()
+        print('DAP adapters:', vim.inspect(vim.tbl_keys(dap.adapters)))
+        print('DAP log file:', dap.get_log_file_path())
+        local js_configs = dap.configurations.javascript or {}
+        print('JavaScript configs:', #js_configs)
+      end, { desc = 'Show DAP status' })
+    end,
+  },
+
+  -- fancy UI for the debugger
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'nvim-neotest/nvim-nio' },
+    keys = {
+      {
+        '<leader>du',
+        function()
+          require('dapui').toggle {}
+        end,
+        desc = 'Dap UI',
+      },
+      {
+        '<leader>de',
+        function()
+          require('dapui').eval()
+        end,
+        desc = 'Eval',
+        mode = { 'n', 'v' },
+      },
+    },
+    opts = {},
+    config = function(_, opts)
+      local dap = require 'dap'
+      local dapui = require 'dapui'
+      dapui.setup(opts)
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open {}
+      end
+      dap.listeners.before.event_terminated['dapui_config'] = function()
+        dapui.close {}
+      end
+      dap.listeners.before.event_exited['dapui_config'] = function()
+        dapui.close {}
+      end
+    end,
+  },
+
+  -- fancy UI for the debugger
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'nvim-neotest/nvim-nio' },
+  -- stylua: ignore
+  keys = {
+    { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+    { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+  },
+    opts = {},
+    config = function(_, opts)
+      local dap = require 'dap'
+      local dapui = require 'dapui'
+      dapui.setup(opts)
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open {}
+      end
+      dap.listeners.before.event_terminated['dapui_config'] = function()
+        dapui.close {}
+      end
+      dap.listeners.before.event_exited['dapui_config'] = function()
+        dapui.close {}
+      end
+    end,
+  },
+
+  -- mason.nvim integration
+  {
+    'jay-babu/mason-nvim-dap.nvim',
+    dependencies = 'mason.nvim',
+    cmd = { 'DapInstall', 'DapUninstall' },
+    opts = {
+      -- Makes a best effort to setup the various debuggers with
+      -- reasonable debug configurations
+      automatic_installation = true,
+
+      -- You can provide additional configuration to the handlers,
+      -- see mason-nvim-dap README for more information
+      handlers = {},
+
+      -- You'll need to check that you have the required things installed
+      -- online, please don't ask me how to install them :)
+      ensure_installed = {
+        -- Update this to ensure that you have the debuggers for the langs you want
+      },
+    },
+    -- mason-nvim-dap is loaded when nvim-dap loads
+    config = function() end,
   },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
