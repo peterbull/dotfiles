645c137bab4b5903411fceafaf34ab154493c250
peter bull <17122363+peterbull@users.noreply.github.com>
2026-08-15 07:28:20 -0400

    updates

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -15,6 +15,14 @@ return {
   opts = {
     notify_on_error = false,
     format_on_save = function(bufnr)
+      -- Skip format-on-save for JS under the ctm repo (~/work/ctm) but not the
+      local ctm_root = vim.fs.normalize(vim.fn.expand '~/work/ctm')
+      local path = vim.fs.normalize(vim.api.nvim_buf_get_name(bufnr))
+      local in_ctm = vim.startswith(path, ctm_root .. '/') or path == ctm_root
+      local is_js = vim.bo[bufnr].filetype == 'javascript' or vim.bo[bufnr].filetype == 'javascriptreact'
+      if in_ctm and is_js and not vim.startswith(path, ctm_root .. '/packages/ctm-ui/') then
+        return nil
+      end
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,10 +1,17 @@
 return {
-	"OXY2DEV/markview.nvim",
-	lazy = false, -- already lazy-loaded internally, don't lazy-load
-	dependencies = {
-		"saghen/blink.cmp", -- callout/checkbox completions
-	},
-	config = function()
-		require("markview").setup()
-	end,
+  {
+    'OXY2DEV/markview.nvim',
+    lazy = false, -- already lazy-loaded internally, don't lazy-load
+    dependencies = {
+      'saghen/blink.cmp', -- callout/checkbox completions
+    },
+    config = function()
+      require('markview').setup()
+    end,
+  },
+  {
+    'kais-radwan/ascii-mermaid',
+    ft = 'markdown',
+    opts = {},
+  },
 }
